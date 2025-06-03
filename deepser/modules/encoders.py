8ebe611eb65c0d3c730243119b087425f5f16913
DimitrisKaratzanos <165014608+DimitrisKaratzanos@users.noreply.github.com>
2025-06-03 23:58:08 +0300

    Update encoders.py

--- deepser/modules/encoders.py
+++ deepser/modules/encoders.py
@@ -13,7 +13,7 @@ class MeanPooling(nn.Module):
 class UnimodalEncoder(nn.Module):
     """Three-layer encoder for each modality following DeepSER structure"""
     def __init__(self, embed_dim, hidden_dim, dropout=0.1):
-        super(MultimodalEncoder, self).__init__()
+        super(UnimodalEncoder, self).__init__()
         
         # Layer 1
         self.layer1_linear = nn.Linear(embed_dim, hidden_dim)
