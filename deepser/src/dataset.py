f327bd0dc1010807a05cfeadd6dbe26eb1f11235
DimitrisKaratzanos <165014608+DimitrisKaratzanos@users.noreply.github.com>
2025-06-19 18:22:59 +0300

    Update dataset.py

--- deepser/src/dataset.py
+++ deepser/src/dataset.py
@@ -28,7 +28,7 @@ class Multimodal_Datasets(Dataset):
         self.audio = dataset[split_type]['audio'].astype(np.float32)
         self.audio[self.audio == -np.inf] = 0
         self.audio = torch.tensor(self.audio).cpu().detach()
-        self.labels = torch.tensor(dataset[split_type]['labels'].astype(np.float32)).cpu().detach()
+        self.labels = torch.tensor(dataset[split_type]['regression_labels'].astype(np.float32)).cpu().detach()
         
         # Note: this is STILL an numpy array
         self.meta = dataset[split_type]['id'] if 'id' in dataset[split_type].keys() else None
@@ -56,4 +56,5 @@ class Multimodal_Datasets(Dataset):
         if self.data == 'iemocap':
             Y = torch.argmax(Y, dim=-1)
         return X, Y, META        
+
 
